7ffda7e61a18cf5eb43fc8864d3fe4d2aa18dbf1
VVS98 <tastorona95@gmail.com>
2025-02-05 22:04:56 +0100

    Added code for lesson-4

---
+++ lesson4/src/loop.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++) {
+    console.log("Iterator from 0 to 9:", i);
+  }
+  
+  for (let i = 100; i >= 0; i -= 10) {
+    console.log("Iterator from 100 to 0 with a step of 10:", i);
+  }
+  
---
+++ lesson4/src/objects.js
@@ -0,0 +1,17 @@
+const complexObject = {
+    name: "Example Object",
+    properties: {
+      type: "complex",
+      version: 1.0,
+    },
+    items: [1, 2, 3, 4, 5],
+    printDetails() {
+      console.log("Name:", this.name);
+      console.log("Type:", this.properties.type);
+      console.log("Version:", this.properties.version);
+      console.log("Items:", this.items);
+    },
+  };
+  
+complexObject.printDetails();
+  
