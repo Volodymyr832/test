6147306e409a041a774c582ca7c25b594a8d283e
VVS98 <tastorona95@gmail.com>
2025-02-18 18:43:04 +0100

    Fix for lesson7

--- lesson7/src/functions.ts
+++ lesson7/src/functions.ts
@@ -1,14 +1,8 @@
 function sumArray(array: (number | string)[]): number {
-    if (!Array.isArray(array)) {
-        console.error('Error: Provided argument is not an array.');
-        return 0;
-    }
-
-    return array.reduce((acc: number, curr: number | string) => acc + (typeof curr === 'number' ? curr : 0), 0);
+    return array.reduce((acc: number, curr: number | string) => 
+        acc + (typeof curr === 'number' ? curr : 0), 0);
 }
 
-const stringArray: string[] = ['hello', 'world', 'test'];
-const numberArray: number[] = [1, 2, 3, 4, 5];
+const mixedArray = [1, 'hello', 2, 'world', 3, 4, 5];
 
-console.log('Sum of numbers in numberArray:', sumArray(numberArray));
-console.log('Sum of elements in stringArray:', sumArray(stringArray));
+console.log('Sum of numbers in mixedArray:', sumArray(mixedArray));
